d39ae16c359a699e401c87f6e6296861777407c5
Steve Michelini <sdmichelini@gmail.com>
2015-04-29 12:22:04 -0400

    Added HWI for Capture/Comparator

--- Lab3/ECE3849d15_Lab3CAN_Cmrypkema_sdmichelin/main.c
+++ Lab3/ECE3849d15_Lab3CAN_Cmrypkema_sdmichelin/main.c
@@ -41,6 +41,23 @@
  *  ======== main ========
  */
 void configureComparator(void);
+void CaptureIsr(void);
+
+#define SAMPLE_SIZE 10
+
+//Init Samples
+//Clears the Sample Buffer
+void InitSamples();
+//Moving-Average Filter
+void AddDataSample(long sample);
+//Get's the average
+long GetAverageSamples();
+
+//Filter Variables
+int g_filterIndex;
+int g_size;
+//Buffer to Hold the Samples
+long g_samples[SAMPLE_SIZE];
 
 Void main()
 { 
@@ -55,8 +72,69 @@ Void main()
 //        System_printf("Task_create() failed!\n");
 //        BIOS_exit(0);
 //    }
+	InitSamples();
 	configureComparator();
     BIOS_start();     /* enable interrupts and start SYS/BIOS */
+}
+
+/*
+HWI for capturing period
+*/
+void CaptureIsr(void){
+	//Clear Interrupt Flag
+	TIMER0_ICR_R = TIMER_ICR_CAECINT;
+
+	static long lastVal = 0;
+
+	if(lastVal == 0){
+		lastVal = TIMER0_TAR_R;//Grab the time if we don't have one
+	}else{
+		long curVal = TIMER0_TAR_R;
+
+		//Catch the rool-over cases by anding w/ 0xffff
+		long diff = (curVal - lastVal)&0xffff;
+
+		//diff is the period in clk ticks
+
+		//Now we need to find a way to average diff
+		//Circular Buffer and adding diff/sizeof(buf) each time???
+		//Is it shared-data safe
+		//Do we care if samples in array get modified
+	}
+}
+
+void InitSamples(){
+	unsigned int i; 
+	for(i = 0; i < SAMPLE_SIZE; i++){
+		g_samples[i] = 0;
+	}
+	g_filterIndex = 0;
+	g_empty = 0;
+}
+
+void AddDataSample(long sample){
+	if(g_filterIndex > SAMPLE_SIZE){
+		g_filterIndex = 0;
+	}	
+
+	g_samples[g_filterIndex] = sample;
+	if(g_size < SAMPLE_SIZE){
+		g_size++;
+	}
+	g_filterIndex++;
+}
+
+long GetAverageSamples(){
+	unsigned int i;
+	long sum = 0;
+	for(i = 0; i < g_size; i++){
+		sum+= g_samples[i];
+	}
+	if(g_size == 0){
+		return 0;
+	}else{
+		return sum/g_size;
+	}
 }
 
 void configureComparator(void){
